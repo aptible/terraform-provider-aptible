02a66c39e7fee83409cb3e07464b0594abb4f907
reggregory <rachel.gregory@aptible.com>
2020-02-27 23:49:37 -0800

    Using structs instead of map[string]interface{} for creating and updating, added Type

--- examples/main.tf
+++ examples/main.tf
@@ -20,6 +20,7 @@ resource "aptible_app" "<app_handle>" {
 resource "aptible_endpoint" "<endpoint_name>" {
   account_id = "<account_id>"
   app_id     = "<app_id>"
+  type = "HTTPS"                    // other options: TCP, TLS
   internal = true                   // or false for external
   container_port = 80               // port #
   ip_filtering = []                 // list of whitelisted IPs
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/go-openapi/runtime v0.19.9
 	github.com/hashicorp/terraform v0.12.18
-	github.com/reggregory/go-deploy v0.0.0-20200226015234-8588a872ae0c
+	github.com/reggregory/go-deploy v0.0.0-20200228072055-1b3e51304662
 )
