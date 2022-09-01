2390d10f937f27741617455e8ba76aec0ef14099
Madhu <madhuravius@protonmail.com>
2022-08-31 22:43:13 -0400

    fix: added basic unit test for common guards on errors

--- aptible/client_utils.go
+++ aptible/client_utils.go
@@ -30,7 +30,23 @@ func GenerateErrorFromClientError(abstractedError interface{}) error {
 	}
 
 	if out.Payload.Code == nil || out.Payload.Error == nil {
-		return fmt.Errorf("Unable to properly decode error (missing fields to properly generate error) - %s\n", err.Error())
+		var errString string
+		if err != nil {
+			errString = fmt.Sprintf("- %s", err.Error())
+		}
+		if out.Payload.Code != nil && *out.Payload.Code >= 400 {
+			errString += fmt.Sprintf(" status code (%d)", *out.Payload.Code)
+		}
+		if out.Payload.Error != nil && len(*out.Payload.Error) > 0 {
+			errString += fmt.Sprintf(" error (%s)", *out.Payload.Error)
+		}
+		if out.Payload.Message != nil && len(*out.Payload.Message) > 0 {
+			errString += fmt.Sprintf(" error message (%s)", *out.Payload.Message)
+		}
+		return fmt.Errorf(
+			"unable to properly decode error (missing fields to properly generate error)%s",
+			errString,
+		)
 	}
 
 	return fmt.Errorf("Error with status code: %d. %s - %s\n", *out.Payload.Code, *out.Payload.Error, *out.Payload.Message)
---
+++ aptible/client_utils_test.go
@@ -0,0 +1,82 @@
+package aptible
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aptible/go-deploy/client/operations"
+	"github.com/aptible/go-deploy/models"
+)
+
+var (
+	badRequestCode          = int64(400)
+	notFoundText            = "not_found"
+	resourceNotFoundMessage = "resource not found"
+)
+
+func TestGenerateErrorFromClientError(t *testing.T) {
+	type args struct {
+		abstractedError interface{}
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantErr   bool
+		errorBody string
+	}{
+		{
+			name: "return a common client error call",
+			args: args{
+				abstractedError: operations.PatchDatabasesIDDefault{
+					Payload: &models.InlineResponseDefault{
+						Code:    &badRequestCode,
+						Error:   &notFoundText,
+						Message: &resourceNotFoundMessage,
+					},
+				},
+			},
+			wantErr: true,
+			errorBody: fmt.Sprintf(
+				"Error with status code: %d. %s - %s\n",
+				badRequestCode,
+				notFoundText,
+				resourceNotFoundMessage,
+			),
+		},
+		{
+			name: "return a pre-baked error when error code not found",
+			args: args{
+				abstractedError: operations.PatchDatabasesIDDefault{
+					Payload: &models.InlineResponseDefault{
+						Code:    nil,
+						Error:   &notFoundText,
+						Message: &resourceNotFoundMessage,
+					},
+				},
+			},
+			wantErr:   true,
+			errorBody: "unable to properly decode error (missing fields to properly generate error) error (not_found) error message (resource not found)",
+		},
+		{
+			name: "return a pre-baked error when error code not found",
+			args: args{
+				abstractedError: operations.PatchDatabasesIDDefault{
+					Payload: &models.InlineResponseDefault{
+						Code:    &badRequestCode,
+						Error:   nil,
+						Message: &resourceNotFoundMessage,
+					},
+				},
+			},
+			wantErr:   true,
+			errorBody: "unable to properly decode error (missing fields to properly generate error) status code (400) error message (resource not found)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GenerateErrorFromClientError(tt.args.abstractedError); (err != nil) == tt.wantErr && tt.errorBody != err.Error() {
+				t.Errorf("GenerateErrorFromClientError() error = %v, wantErr %v, errorBody %s", err, tt.wantErr, tt.errorBody)
+			}
+		})
+	}
+}
